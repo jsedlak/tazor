854983717f6cd1f249504fcd14b8441776cbe6d8
John Sedlak <john.sedlak@outlook.com>
2022-12-12 22:56:21 -0500

    Adds ToSlug extension

---
+++ src/Tazor.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Tazor.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToSlug(this string value)
+    {
+        return value
+            .Replace(" ", "-")
+            .Replace("\t", "")
+            .Replace("\r", "")
+            .Replace("\n", "")
+            .ToLowerInvariant();
+    }
+}
