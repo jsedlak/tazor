7d6a6053c9ff2c78e4a4a6cc32d44be3a80aa51d
John Sedlak <john.sedlak@outlook.com>
2023-07-23 07:25:19 -0400

    Adds some string extensions

--- src/Tazor.Core/Extensions/StringExtensions.cs
+++ src/Tazor.Core/Extensions/StringExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Tazor.Extensions;
+﻿using System.Text.RegularExpressions;
+
+namespace Tazor.Extensions;
 
 public static class StringExtensions
 {
@@ -10,5 +12,30 @@ public static class StringExtensions
             .Replace("\r", "")
             .Replace("\n", "")
             .ToLowerInvariant();
+    }
+
+    public static string CamelCase(this string s)
+    {
+        var x = s.Replace("_", "");
+        if (x.Length == 0) return "null";
+        x = Regex.Replace(x, "([A-Z])([A-Z]+)($|[A-Z])",
+            m => m.Groups[1].Value + m.Groups[2].Value.ToLower() + m.Groups[3].Value);
+        return char.ToLower(x[0]) + x.Substring(1);
+    }
+
+    public static string PascalCase(this string s)
+    {
+        var x = CamelCase(s);
+        return char.ToUpper(x[0]) + x.Substring(1);
+    }
+
+    public static string TrimStart(this string input, string trim)
+    {
+        if (!input.StartsWith(trim, StringComparison.OrdinalIgnoreCase))
+        {
+            return input;
+        }
+
+        return input.Substring(trim.Length);
     }
 }
