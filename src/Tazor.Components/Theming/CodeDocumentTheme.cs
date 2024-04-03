ece8444ce6c06784efe5f57824f911e96a4c249d
John Sedlak <john.sedlak@outlook.com>
2024-04-02 22:21:36 -0400

    Updates all components to use new cascading parameter theme

---
+++ src/Tazor.Components/Theming/CodeDocumentTheme.cs
@@ -0,0 +1,22 @@
+﻿namespace Tazor.Components.Theming;
+
+/// <summary>
+/// Provides theming for the CodeBlock component
+/// </summary>
+public class CodeBlockTheme
+{
+    /// <summary>
+    /// Gets or Sets the wrapper class
+    /// </summary>
+    public string Wrapper { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or Sets the header class
+    /// </summary>
+    public string Header { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or Sets the code block class
+    /// </summary>
+    public string Pre { get; set; } = string.Empty;
+}
