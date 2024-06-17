84440657a4990c84d631235e4f6139c27999d740
John Sedlak <john.sedlak@outlook.com>
2024-06-17 08:06:06 -0400

    Adds styling for OmniBox

--- src/Tazor.Components/Theming/FormsTheme.cs
+++ src/Tazor.Components/Theming/FormsTheme.cs
@@ -29,75 +29,9 @@ public class FormsTheme
     /// Gets or Sets the theming for the ComboBox component
     /// </summary>
     public ComboBoxTheme ComboBox { get; set; } = new();
-}
-
-/// <summary>
-/// Provides theming for the ComboBox component
-/// </summary>
-public class ComboBoxTheme
-{
-    /// <summary>
-    /// Gets or Sets the wrapper CSS classes
-    /// </summary>
-    public string Wrapper { get; set; } = string.Empty;
 
     /// <summary>
-    /// Gets or Sets the CSS classes for the container holding selected items
+    /// Gets or Sets theming info for the OmniBox component
     /// </summary>
-    public string ViewContainer { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets or Sets the CSS classes for the list of search results
-    /// </summary>
-    public string ViewList { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets or Sets the CSS classes for the list item
-    /// </summary>
-    public string ViewListItem { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets or Sets the CSS classes for the container around the input
-    /// </summary>
-    public string InputContainer { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets or Sets the CSS classes for the input itself
-    /// </summary>
-    public string Input { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets or Sets the CSS classes for the dropdown button
-    /// </summary>
-    public string DropdownButton { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets or Sets the CSS class for the icon inside the button
-    /// </summary>
-    public string DropdownButtonIcon { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets or Sets the CSS classes for the dropdown container
-    /// </summary>
-    public string DropdownContainer { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets or Sets the CSS classes for the dropdown list
-    /// </summary>
-    public string DropdownList { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets or Sets the CSS classes for the dropdown item
-    /// </summary>
-    public string DropdownItem { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets or Sets the CSS classes for the checkbox for selecting items
-    /// </summary>
-    public string DropdownCheckbox { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Gets or Sets the CSS classes for the content of the dropdown item
-    /// </summary>
-    public string DropdownItemContent { get; set; } = string.Empty;
+    public OmniBoxTheme OmniBox { get; set; } = new();
 }
